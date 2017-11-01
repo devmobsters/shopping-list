3431cb2c89151239dffc2260af9a72167a3ea176
Valantis Koutsoumpos <chrysovalantis.koutsoumpos@devmob.com>
2017-11-01 11:10:35 +0100

    Changed the action of the add product form

--- resources/views/addproduct.blade.php
+++ resources/views/addproduct.blade.php
@@ -8,7 +8,7 @@
                     <div class="panel-heading">Add Product</div>
 
                     <div class="panel-body">
-                        <form class="form-horizontal" method="POST" action="/products">
+                        <form class="form-horizontal" method="POST" action="/products/store">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
